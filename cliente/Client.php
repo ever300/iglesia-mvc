b25ba037e21af401b8bbd057f91f28761de6b38f
Ever_Ochoa_8a <78324213+ever300@users.noreply.github.com>
2025-06-14 22:31:48 -0400

    Add ControllerFactory for controller creation

--- cliente/Client.php
+++ cliente/Client.php
@@ -1,25 +1,20 @@
 <?php
 require_once __DIR__ . '/../config/conexion.php';
-require_once __DIR__ . '/../controlador/AuthProxy.php';
+require_once __DIR__ . '/ControllerFactory.php';
 
 class Client {
+    private $factory;
+
+    public function __construct() {
+        $this->factory = new ControllerFactory();
+    }
+
     public function handle() {
         $controller = $_GET['controller'] ?? null;
         if ($controller) {
-            $controllerClass = 'C' . ucfirst($controller);
-            $controllerFile = __DIR__ . '/../controlador/' . $controllerClass . '.php';
-
-            if (file_exists($controllerFile)) {
-                require_once $controllerFile;
-                $controllerInstance = new $controllerClass();
-                if ($controller !== 'auth') {
-                    $controllerInstance = new AuthProxy($controllerInstance);
-                }
-                $controllerInstance->handleRequest();
-                return true;
-            } else {
-                die("❌ Controlador no encontrado: $controllerClass");
-            }
+            $controllerInstance = $this->factory->create($controller);
+            $controllerInstance->handleRequest();
+            return true;
         }
         return false;
     }
